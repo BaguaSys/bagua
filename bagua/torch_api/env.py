6f77705104358a586c459a9e1da724fbb4983cc3
shjwudp <shjwudp@gmail.com>
2021-06-10 21:56:44 +0800

    docs: refactor bagua.torch_api interface; remove unnecessarg API doc (#10)

--- bagua/torch_api/env.py
+++ bagua/torch_api/env.py
@@ -2,18 +2,53 @@ import os
 
 
 def get_world_size():
+    """
+    Returns the number of processes in the current process group
+
+    Returns:
+        The world size of the process group
+
+    """
     return int(os.environ.get("WORLD_SIZE", 1))
 
 
 def get_rank():
+    """
+    Returns the rank of current process group
+
+    Rank is a unique identifier assigned to each process within a distributed
+    process group. They are always consecutive integers ranging from 0 to
+    ``world_size``.
+
+    Returns:
+        The rank of the process group
+
+    """
     return int(os.environ.get("RANK", 0))
 
 
 def get_local_rank():
+    """
+    Returns the rank of current node
+
+    Rank is a unique identifier assigned to each process within a node.
+    They are always consecutive integers ranging from 0 to ``local_size``.
+
+    Returns:
+        The local rank of the node
+
+    """
     return int(os.environ.get("LOCAL_RANK", 0))
 
 
 def get_local_size():
+    """
+    Returns the number of processes in the node
+
+    Returns:
+        The local size of the node
+
+    """
     return int(os.environ.get("LOCAL_SIZE", 1))
 
 
