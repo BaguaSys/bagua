14f1f0ab3ebb81542cb8089a55719d717eca8229
Xiangru Lian <admin@mail.xrlian.com>
2021-06-10 20:35:20 +0800

    docs: remove duplicated sphinx options

--- conf.py
+++ conf.py
@@ -53,11 +53,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "alabaster"
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
@@ -72,7 +67,6 @@ add_module_names = True
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
-
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
