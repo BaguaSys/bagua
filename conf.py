d64f65d23b56ebb46c59d9970593c875930236a0
Xiangru Lian <admin@mail.xrlian.com>
2021-06-10 16:43:24 +0800

    docs: change sphinx html theme

--- conf.py
+++ conf.py
@@ -33,8 +33,12 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon"
 ]
 
+
+napoleon_numpy_docstring = True
+autodoc_typehints = 'description'
 autoapi_type = "python"
 autoapi_dirs = ["./bagua"]
 
@@ -72,7 +76,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "classic"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
