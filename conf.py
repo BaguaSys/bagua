57c0aed8f92955b4060bd78a7ed213073ee1d2e0
NOBLES5E <NOBLES5E@users.noreply.github.com>
2021-06-10 08:39:17 +0000

    :art: Format Python code with psf/black

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ author = "Kuaishou AI Platform and DS3 Lab"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'autoapi.extension',
+    "autoapi.extension",
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
@@ -36,7 +36,7 @@ extensions = [
 ]
 
 autoapi_type = "python"
-autoapi_dirs = ['./bagua']
+autoapi_dirs = ["./bagua"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -57,7 +57,7 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = False
