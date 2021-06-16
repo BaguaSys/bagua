4d62380fb89c69a674b96b87be0bfd4f79b4a022
Xiangru Lian <admin@mail.xrlian.com>
2021-06-16 05:15:35 -0700

    update

--- bagua/torch_api/distributed_dev.py
+++ bagua/torch_api/distributed_dev.py
@@ -15,27 +15,25 @@ class Algorithm:
         pass
 
 
-def _get_module_params_and_buffers(module):
-    # TODO: document this
-    if hasattr(module, "_ddp_params_and_buffers_to_ignore"):
-        parameters_to_ignore = module._ddp_params_and_buffers_to_ignore
-    else:
-        parameters_to_ignore = []
-    module_states = []
-    for name, param in module.state_dict().items():
-        if name not in parameters_to_ignore:
-            module_states.append(param)
-    return module_states
-
-
-def broadcast_parameters(module):
-    module_states = _get_module_params_and_buffers(module)
-    authoritative_rank = 0
-    for state in module_states:
-        broadcast(state, root=authoritative_rank)
-
-
 class DistributedWrapper():
+    def _get_module_params_and_buffers(module):
+        # TODO: document this
+        if hasattr(module, "_ddp_params_and_buffers_to_ignore"):
+            parameters_to_ignore = module._ddp_params_and_buffers_to_ignore
+        else:
+            parameters_to_ignore = []
+        module_states = []
+        for name, param in module.state_dict().items():
+            if name not in parameters_to_ignore:
+                module_states.append(param)
+        return module_states
+
+    def _broadcast_parameters(module):
+        module_states = DistributedWrapper._get_module_params_and_buffers(module)
+        authoritative_rank = 0
+        for state in module_states:
+            broadcast(state, root=authoritative_rank)
+
     def __init__(self, module, optimizer, algorithm):
         self.module = module
         self.optimizer = optimizer
@@ -46,7 +44,7 @@ class DistributedWrapper():
         self.intra_node_communicator = _get_global_state().get_intranode_communicator()
         self.global_communicator = _get_global_state().get_global_communicator()
 
-        broadcast_parameters(self.module)
+        DistributedWrapper._broadcast_parameters(self.module)
         # TODO: broadcast optimizer parameters
 
         self.buckets = self.algorithm.register_buckets(self.module, self.optimizer)
