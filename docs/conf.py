4437344ef24505719504ee5976b5eb8a32f3774c
Xiangru Lian <admin@mail.xrlian.com>
2021-06-18 13:45:44 -0700

    update

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ autoapi_ignore = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ["_autoapi_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
