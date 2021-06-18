448e51a7626e4a0783ff37b92aa533d3b2f0e631
shjwudp <shjwudp@gmail.com>
2021-06-18 14:50:30 +0800

    ..

--- bagua/distributed/run.py
+++ bagua/distributed/run.py
@@ -162,7 +162,7 @@ from argparse import REMAINDER, ArgumentParser
 from typing import Callable, List, Tuple, Union
 
 import torch
-from bagua.distributed.argparse_util import check_env, env
+from torch.distributed.argparse_util import check_env, env
 from torch.distributed.elastic.multiprocessing import Std
 from torch.distributed.elastic.rendezvous.utils import _parse_rendezvous_config
 from torch.distributed.elastic.utils import macros
