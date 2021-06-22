4213a8125cf26112577530ed8db4206ab6507030
suntengxu <suntengxu21@163.com>
2021-06-22 17:10:36 +0800

    add import module in init.py

--- bagua/torch_api/__init__.py
+++ bagua/torch_api/__init__.py
@@ -2,7 +2,6 @@
 """
 The Bagua communication library PyTorch interface.
 """
-from .communication import init_process_group
 from .distributed import bagua_init
 from .env import (
     get_rank,
@@ -10,6 +9,17 @@ from .env import (
     get_local_rank,
     get_local_size,
 )
+from .communication import (
+    is_initialized,
+    init_process_group,
+    broadcast_coalesced,
+    broadcast,
+    allreduce_coalesced,
+    allreduce,
+    get_bagua_hyperparameters,
+    get_hyperparameters_service_client,
+)
+
 from .fuse_optimizer import FusedOptimizer
 from ..bagua_define import DistributedAlgorithm
 import bagua.torch_api.contrib
