7693db0234095ac41b44882226b0791bcdd20231
Tengxu-Sun <52795221+Tengxu-Sun@users.noreply.github.com>
2021-06-22 18:19:10 +0800

    fix: add back user interfacing imports in init.py (#38)

--- bagua/torch_api/__init__.py
+++ bagua/torch_api/__init__.py
@@ -2,13 +2,17 @@
 """
 The Bagua communication library PyTorch interface.
 """
-from .communication import init_process_group
 from .distributed import bagua_init
 from .env import (
     get_rank,
     get_world_size,
     get_local_rank,
     get_local_size,
+)
+from .communication import (  # noqa: F401
+    init_process_group,
+    broadcast,
+    allreduce,
 )
 from .fuse_optimizer import FusedOptimizer
 from ..bagua_define import DistributedAlgorithm
