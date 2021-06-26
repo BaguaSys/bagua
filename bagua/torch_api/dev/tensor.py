0e0c622564bb586d90bd20301bc0f80c2cc5c863
Xiangru Lian <admin@mail.xrlian.com>
2021-06-26 20:36:11 +0800

    remove register from tensor

--- bagua/torch_api/dev/tensor.py
+++ bagua/torch_api/dev/tensor.py
@@ -6,16 +6,6 @@ class BaguaTensor(torch.Tensor):
     def __init__(self, original_tensor: torch.Tensor) -> None:
         super().__init__()
         self.inner = original_tensor
-        self.bagua_backend = None
-
-    def register(self, bagua_backend):
-        assert self.bagua_backend is None
-        self.bagua_backend = bagua_backend
-        ...
-
-    def is_registered(self) -> bool:
-        return not (self.bagua_backend is None)
-
 
     def mark_communication_ready(self):
         ...
