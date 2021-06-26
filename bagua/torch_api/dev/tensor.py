5240dd4c96690d67dcaa3baf9bc2dbcbcd94d8a4
Xiangru Lian <admin@mail.xrlian.com>
2021-06-26 19:56:46 +0800

    update bagua tensor class

--- bagua/torch_api/dev/tensor.py
+++ bagua/torch_api/dev/tensor.py
@@ -6,3 +6,12 @@ class BaguaTensor(torch.Tensor):
     def __init__(self, original_tensor: torch.Tensor) -> None:
         super().__init__()
         self.inner = original_tensor
+        self.bagua_backend = None
+
+    def register(self, bagua_backend):
+        assert self.bagua_backend is None
+        self.bagua_backend = bagua_backend
+        ...
+
+    def is_registered(self) -> bool:
+        return not (self.bagua_backend is None)
