4e9482fbf987bec2c4fd377a7dbebba377865cff
Xiangru Lian <admin@mail.xrlian.com>
2021-06-26 20:34:11 +0800

    remove register from BaguaBucket

--- bagua/torch_api/dev/bucket.py
+++ bagua/torch_api/dev/bucket.py
@@ -7,14 +7,7 @@ from typing import List
 class BaguaBucket:
     def __init__(self, tensors: List[BaguaTensor]) -> None:
         self.tensors = tensors
-
-    def register(self, bagua_backend):
-        assert self.bagua_backend is None
-        self.bagua_backend = bagua_backend
-        ...
-
-    def is_registered(self) -> bool:
-        return not (self.bagua_backend is None)
+        self.backend_tensor = None
 
     def flatten_(self):
         ...
