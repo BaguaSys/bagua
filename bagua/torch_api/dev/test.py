943d890f32cd12927ae52e651acb729c762d80fb
Xiangru Lian <admin@mail.xrlian.com>
2021-06-27 22:47:50 +0800

    udpate

--- bagua/torch_api/dev/test.py
+++ bagua/torch_api/dev/test.py
@@ -4,7 +4,7 @@ You can compare the performance with the horovod script directly.
 """
 
 import argparse
-from bagua.torch_api.dev.algorithms import Algorithm, DevelopAlgoritm
+from bagua.torch_api.dev.algorithms import Algorithm, DevelopAlgorithm
 from bagua.torch_api.dev.distributed_dev import DistributedWrapper
 import bagua.torch_api.dev.tensor
 import bagua.torch_api.dev.distributed_dev
