97e5bb005ce5838657c698ba255851de451442ea
Shaoduo <ganshaoduo@gmail.com>
2021-06-27 13:26:30 +0200

    bucket flatten

--- bagua/torch_api/dev/bucket.py
+++ bagua/torch_api/dev/bucket.py
@@ -2,6 +2,7 @@
 
 from bagua.torch_api.dev.tensor import BaguaTensor
 from typing import List
+from bagua.torch_api.utils import check_contiguous
 import torch
 
 
@@ -9,14 +10,33 @@ class BaguaBucket:
     def __init__(self, tensors: List[BaguaTensor]) -> None:
         self.tensors = tensors
         self.backend_tensor = None
-
+        self.is_flattened = False
 
     def flatten_(self):
-        # TODO: finish me @shaoduo
         """
         flatten inner tensors in place
         """
+        if len(self.tensors) == 0:
+            return
+        total_size = 0
+        for tensor in self.tensors:
+            total_size += tensor.numel()
+
+        flatten_tensor = torch.zeros(total_size, dtype=self.tensors[0].dtype).to(
+            self.tensors[0].device
+        )
+        flatten_storage = flatten_tensor.storage()
+
+        offset = 0
+        for tensor in self.tensors:
+            # copy data
+            flatten_tensor[offset : offset + tensor.numel()] = tensor.data.reshape(-1)
+            tensor.set_storage(flatten_storage, offset)
+            offset += tensor.numel()
+        # check
+        assert check_contiguous([tensor for tensor in self.tensors])
+        self.is_flattened = True
 
     def is_flatten(self) -> bool:
-        # TODO: finish me @shaoduo
-        ...
+        return self.is_flattened
+
