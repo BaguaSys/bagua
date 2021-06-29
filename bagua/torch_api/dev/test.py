4c4f9762ba4b4c85fcc3a15c64c9563c46ea1716
Xiangru Lian <admin@mail.xrlian.com>
2021-06-28 20:17:19 -0700

    update

--- bagua/torch_api/dev/test.py
+++ bagua/torch_api/dev/test.py
@@ -112,8 +112,8 @@ optimizer = bagua.contrib.FusedOptimizer(optimizer)
 # optimizer = optim.SGD(model.parameters(), lr=0.01 * bagua.get_world_size())
 # optimizer = OnebitAdamOptimizer(model.parameters())
 
-# model.with_bagua([optimizer], algorithm=ByteGradAlgorithm())
-model.with_bagua([optimizer], algorithm=OnebitAdamAlgorithm())
+model.with_bagua([optimizer], algorithm=ByteGradAlgorithm())
+# model.with_bagua([optimizer], algorithm=OnebitAdamAlgorithm())
 
 # Set up fixed fake data
 data = torch.randn(args.batch_size, 3, 224, 224)
