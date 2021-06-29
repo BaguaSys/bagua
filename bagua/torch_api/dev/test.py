fbee1d0472ddd5ffda659129dc09ae57fe4130fa
Shaoduo <ganshaoduo@gmail.com>
2021-06-29 22:17:59 +0200

    1

--- bagua/torch_api/dev/test.py
+++ bagua/torch_api/dev/test.py
@@ -113,8 +113,7 @@ if args.cuda:
 optimizer = OnebitAdamOptimizer(model.parameters())
 
 # model.with_bagua([optimizer], algorithm=ByteGradAlgorithm())
-dis_algorithm = OnebitAdamAlgorithm(model, 5)
-model.with_bagua([optimizer], algorithm=dis_algorithm)
+model.with_bagua([optimizer], algorithm=OnebitAdamAlgorithm(model, 5))
 
 # Set up fixed fake data
 data = torch.randn(args.batch_size, 3, 224, 224)
