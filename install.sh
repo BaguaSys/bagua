0e50e3d41c567b086b16547052ef41ca7025ca67
liuchengjun <liuchengjun@kuaishou.com>
2021-06-30 17:14:51 +0800

    fix: auto installation for centos

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -eu
+set -eux
 
 exit_and_error() {
   echo "Auto installation is supported only on Ubuntu(16.04,18.04,20.04) or CentOs(7,8), abort."
@@ -10,11 +10,11 @@ check_os_version() {
   OS_NAME=`grep ^NAME /etc/os-release | awk -F'"' '{print $2}'`
   VERSION_ID=`grep ^VERSION_ID /etc/os-release | awk -F'"' '{print $2}'`
   echo "Current OS is "${OS_NAME}", Version is "${VERSION_ID}
-  if [ $OS_NAME == "Ubuntu" ]; then
+  if [ "$OS_NAME" == "Ubuntu" ]; then
     if [[ $VERSION_ID != @("16.04"|"18.04"|"20.04") ]]; then
       exit_and_error
     fi
-  elif [ $OS_NAME == "CentOS Linux" ]; then
+  elif [ "$OS_NAME" == "CentOS Linux" ]; then
     if [[ $VERSION_ID != @("7"|"8") ]]; then
       exit_and_error
     fi
@@ -27,7 +27,7 @@ check_os_version() {
 check_os_version
 
 
-if [ $OS_NAME == "Ubuntu" ]; then
+if [ "$OS_NAME" == "Ubuntu" ]; then
   # install cmake & python3-pip
   apt-get update && apt-get install -y curl software-properties-common wget
   wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
@@ -46,11 +46,12 @@ if [ $OS_NAME == "Ubuntu" ]; then
 
   # install zlib, ssl, openmpi
   apt-get install -y zlib1g-dev libssl-dev openmpi-bin openmpi-doc libopenmpi-dev
-elif [ $OS_NAME == "CentOS Linux" ]; then
+elif [ "$OS_NAME" == "CentOS Linux" ]; then
   if [ $VERSION_ID == "7" ]; then
     yum install centos-release-scl-rh -y && yum install devtoolset-8-toolchain -y
-    scl enable devtoolset-8 bash
-    yum install http://repo.okay.com.mx/centos/7/x86_64/release/okay-release-1-1.noarch.rpm -y
+    #scl enable devtoolset-8 bash
+    source /opt/rh/devtoolset-8/enable
+    yum remove okay-release -y && yum install http://repo.okay.com.mx/centos/7/x86_64/release/okay-release-1-1.noarch.rpm -y
     yum remove cmake3 -y && yum install cmake3 -y
     yum install -y openmpi3-devel hwloc-devel
     ln -sf /usr/bin/cmake3 /usr/bin/cmake
@@ -72,5 +73,5 @@ export PATH="$HOME/.cargo/bin:$PATH"
 
 # install bagua
 #python3 -m pip install bagua -f https://repo.arrayfire.com/python/wheels/3.8.0/
-python3 -m pip install bagua-core==0.2.2.dev15 -f https://repo.arrayfire.com/python/wheels/3.8.0/ -i https://pypi.org/simple
+python3 -m pip install bagua-core==0.2.2.dev18 -f https://repo.arrayfire.com/python/wheels/3.8.0/ -i https://pypi.org/simple
 python3 -m pip install bagua==0.4.1.dev26 -f https://repo.arrayfire.com/python/wheels/3.8.0/ -i https://pypi.org/simple
