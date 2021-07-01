96cb6fe72dfcb2d0394e465291a31aff1f3e0142
Xiangru Lian <admin@mail.xrlian.com>
2021-07-01 13:06:24 +0800

    require bagua-core 0.3 now

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         author="Kuaishou AI Platform & DS3 Lab",
         author_email="admin@mail.xrlian.com",
         install_requires=[
-            "bagua-core>=0.2,<0.3",
+            "bagua-core>=0.3,<0.4",
             "deprecation",
             "pytest-benchmark",
             "scikit-optimize",
