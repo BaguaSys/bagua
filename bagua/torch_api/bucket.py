6a750cb864084f1c44394a200d39910a5f916ffc
Xiangru Lian <admin@mail.xrlian.com>
2021-07-02 02:56:14 +0800

    add ops to python class

--- bagua/torch_api/bucket.py
+++ bagua/torch_api/bucket.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-from typing import List
+from __future__ import annotations
+from typing import List, Callable, Optional
 
 import bagua_core as B
 import torch
+from bagua.torch_api.globals import _get_global_state
+
 from bagua.torch_api.tensor import BaguaTensor
 from bagua.torch_api.utils import check_contiguous
 
 
 class BaguaBucket:
     def __init__(
-        self, tensors: List[BaguaTensor], name: str, flatten: bool, alignment: int = 1
+            self, tensors: List[BaguaTensor], name: str, flatten: bool, alignment: int = 1
     ) -> None:
         """
         Create a Bagua bucket with a list of Bagua tensors.
@@ -73,7 +76,7 @@ class BaguaBucket:
         offset = 0
         for tensor in self.tensors:
             # copy data
-            flatten_tensor[offset : offset + tensor.numel()] = tensor.data.reshape(-1)
+            flatten_tensor[offset: offset + tensor.numel()] = tensor.data.reshape(-1)
             tensor.bagua_set_storage(flatten_storage, offset)
             offset += tensor.numel()
         # check
@@ -85,3 +88,98 @@ class BaguaBucket:
             True if the bucket's tensors are contiguous in memory.
         """
         return check_contiguous(self.tensors)
+
+    def append_python_op(self, python_function: Callable[[str], None]) -> BaguaBucket:
+        """
+        Append a Python operation to a bucket. A Python operation is a Python function that
+        takes the bucket's name and returns ``None``. It can do arbitrary things within the
+        function body.
+
+        The operations will be executed by the Bagua backend in the order they are appended
+        when all the tensors within the bucket are marked ready.
+
+        Args:
+            python_function: The Python operation function.
+
+        Returns:
+            The bucket itself.
+        """
+
+        self.backend_bucket.append_python_op(python_function)
+        return self
+
+    def append_centralized_synchronous_op(self, hierarchical: bool = False,
+                                          average: bool = True, scattergather: bool = False,
+                                          compression: Optional[str] = None) -> BaguaBucket:
+        """
+        Append a centralized synchronous operation to a bucket. It will sum or average the tensors in the bucket
+        for all workers.
+
+        The operations will be executed by the Bagua backend in the order they are appended
+        when all the tensors within the bucket are marked ready.
+
+        Args:
+            hierarchical (bool): Enable hierarchical communication. Which means the GPUs on the same machine
+                will communicate will each other first. After that, machines do inter-node communication. This can
+                boost performance when the inter-node communication cost is high.
+            average (bool): If True, the gradients on each worker are averaged. Otherwise, they are summed.
+            scattergather (bool): If true, the communication between workers are done with scatter gather instead
+                of allreduce. This is required for using compression.
+            compression: If not None, the tensors will be compressed for communication. Currently "MinMaxUInt8" is
+                supported.
+        Returns:
+            The bucket itself.
+        """
+        if hierarchical:
+            self.backend_bucket.append_centralized_synchronous_op(
+                _get_global_state().get_internode_communicator(),
+                _get_global_state().get_intranode_communicator(),
+                hierarchical=hierarchical,
+                average=average,
+                scattergather=scattergather,
+                compression=compression,
+            )
+        else:
+            self.backend_bucket.append_centralized_synchronous_op(
+                _get_global_state().get_global_communitor(),
+                None,
+                hierarchical=hierarchical,
+                average=average,
+                scattergather=scattergather,
+                compression=compression,
+            )
+        return self
+
+    def append_decentralized_synchronous_op(self, hierarchical: bool =True, peer_selection_mode: str="all", communication_interval: int =1) -> BaguaBucket:
+        """
+        Append a decentralized synchronous operation to a bucket. It will do gossipy style model averaging among workers.
+
+        The operations will be executed by the Bagua backend in the order they are appended
+        when all the tensors within the bucket are marked ready.
+
+        Args:
+            hierarchical (bool): Enable hierarchical communication. Which means the GPUs on the same machine
+                will communicate will each other first. After that, machines do inter-node communication. This can
+                boost performance when the inter-node communication cost is high.
+            peer_selection_mode (str): Can be "all" or "shift_one". "all" means all workers'
+                weights are averaged in each communication step. "shift_one" means each worker
+                selects a different peer to do weights average in each communication step.
+            communication_interval (int): Number of iterations between two communication steps.
+        Returns:
+            The bucket itself.
+        """
+        self.backend_bucket.append_decentralized_synchronous_op(
+            _get_global_state().get_internode_communicator(),
+            _get_global_state().get_intranode_communicator(),
+            hierarchical=hierarchical,
+            compression=None,
+            peer_selection_mode=peer_selection_mode,
+            communication_interval=communication_interval,
+        )
+        return self
+
+    def clear_ops(self) -> BaguaBucket:
+        """
+        Clear the previously appended operations.
+        """
+        self.backend_bucket.clear_ops()
