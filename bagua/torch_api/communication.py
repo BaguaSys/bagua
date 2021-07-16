a0dda68eb7ec6407660ed2809f9c918b9142e1b6
liuhatry <liuhatry@163.com>
2021-07-16 16:45:33 +0800

    Update bagua/torch_api/communication.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- bagua/torch_api/communication.py
+++ bagua/torch_api/communication.py
@@ -647,8 +647,12 @@ def scatter(
             Defaults to None.
     """
 
-    assert send_tensor.device != torch.device("cpu"), "send tensor must be CUDA and dense"
-    assert recv_tensor.device != torch.device("cpu"), "recv tensor must be CUDA and dense"
+    assert send_tensor.device != torch.device(
+        "cpu"
+    ), "send tensor must be CUDA and dense"
+    assert recv_tensor.device != torch.device(
+        "cpu"
+    ), "recv tensor must be CUDA and dense"
 
     if comm is None:
         comm = get_backend("").global_communicator
