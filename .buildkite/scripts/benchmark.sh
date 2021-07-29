215836b9172ad276289e6de5eb33dcfd5c783037
shjwudp <shjwudp@gmail.com>
2021-07-29 12:46:56 +0800

    ..

---
+++ .buildkite/scripts/benchmark.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "$BUILDKITE_PARALLEL_JOB"
+echo "$BUILDKITE_PARALLEL_JOB_COUNT"
+
+set -euox pipefail
+
+SYNTHETIC_SCRIPT="/bagua/examples/benchmark/synthetic_benchmark.py"
+
+function parse_benchmark_log {
+    logfile=$1
+
+    img_per_sec=$(cat ${logfile} | grep "Img/sec per " | tail -n 1 | awk '{print $5}')
+
+    echo $img_per_sec
+}
+
+logfile=$(mktemp /tmp/bagua_benchmark.XXXXXX.log)
+python -m bagua.distributed.run \
+    --standalone \
+    --nnodes=1 \
+    --nproc_per_node 8 \
+    --no_python \
+    --autotune_level 1 \
+    python ${SYNTHETIC_SCRIPT} \
+    2>&1 | tee ${logfile}
+parse_benchmark_log ${logfile}
