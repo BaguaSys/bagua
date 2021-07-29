1d1421e1fdd53d68b6eb5c8cdce414082dcef6df
shjwudp <shjwudp@gmail.com>
2021-07-29 11:42:52 +0800

    test: add autotune end2end test

---
+++ .buildkite/scripts/benchmark.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "$BUILDKITE_PARALLEL_JOB"
+echo "$BUILDKITE_PARALLEL_JOB_COUNT"
+
+set -euo pipefail
+
+SYNTHETIC_SCRIPT="/bagua/examples/benchmark/synthetic_benchmark.py"
+
+function parse_training_log {
+    logfile=$1
+
+    img_per_sec=$(cat ${logfile} | grep "Img/sec per " | tail -n 1 | awk '{print $4}')
+
+    echo $img_per_sec
+}
+
+logfile=$(mktemp /tmp/bagua_benchmark.XXXXXX.log)
+baguarun --nproc_per_node 8 \
+    --host_list localhost \
+    --no_python \
+    "--autotune_level 1" \
+    python ${SYNTHETIC_SCRIPT} \
+    2> &1 | tee ${logfile}
+parse_training_log ${logfile}
