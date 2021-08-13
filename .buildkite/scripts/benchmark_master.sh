896bfded802fdd3d61878e685c9771d87f9dbb7f
liuhatry <liuhatry@163.com>
2021-08-13 11:17:28 +0800

    update

--- .buildkite/scripts/benchmark_master.sh
+++ .buildkite/scripts/benchmark_master.sh
@@ -23,12 +23,12 @@ function check_benchmark_log {
         echo $result
         CHECK_RESULT[${#CHECK_RESULT[*]}]=$result
     fi
-    if [ $img_per_sec -lt $speed ]; then
+    if [ 1 -eq "$(echo "$img_per_sec < $speed" | bc)" ]; then
         result="Check ["${algorithm}"] fail, img_per_sec is less than "$speed
         echo $result
         CHECK_RESULT[${#CHECK_RESULT[*]}]=$result
     else
-        echo "Check ["${algorithm}"] success, img_per_sec is greater than"$speed
+        echo "Check ["${algorithm}"] success, img_per_sec is greater than "$speed
     fi
 
     #python -c "import sys; sys.exit(1) if float($final_batch_loss) != float($loss) else print('final_batch_loss is euqal.')"
