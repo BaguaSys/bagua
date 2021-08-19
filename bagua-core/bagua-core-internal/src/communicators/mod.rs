64d5cf0a4cf0c147cc1a25b3915ebef1689ff5cd
wangraying <45031995+wangraying@users.noreply.github.com>
2021-08-19 17:13:10 +0800

    fix: replace mutex with atomic bool for async op and add Aluminum submodule update (#67)

--- bagua-core/bagua-core-internal/src/communicators/mod.rs
+++ bagua-core/bagua-core-internal/src/communicators/mod.rs
@@ -3,7 +3,7 @@ use crate::datatypes::{
 };
 use crate::BaguaCoreError;
 use itertools::Itertools;
-use parking_lot::Mutex;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
 #[derive(Clone, Debug)]
@@ -13,7 +13,7 @@ pub struct BaguaCommunicatorInner {
     pub rank: usize,
     pub nranks: usize,
     pub device_id: usize,
-    pub aborted: Arc<Mutex<bool>>,
+    pub aborted: Arc<AtomicBool>,
 }
 
 #[derive(Clone, Debug)]
@@ -54,7 +54,7 @@ impl BaguaSingleCommunicator {
                 rank,
                 nranks,
                 device_id,
-                aborted: Arc::new(Mutex::new(false)),
+                aborted: Arc::new(AtomicBool::new(false)),
             }),
         }
     }
@@ -456,7 +456,8 @@ impl BaguaCommunicatorInner {
     pub fn abort(&self) {
         let communicator_ptr = self.comm_ptr;
 
-        *self.aborted.lock() = true;
+        self.aborted.store(true, Ordering::Relaxed);
+
         unsafe {
             cpp::cpp!([communicator_ptr as "Al::NCCLCommunicator*"]
             {
@@ -466,7 +467,7 @@ impl BaguaCommunicatorInner {
     }
 
     pub fn check_abort(&self) -> bool {
-        *self.aborted.lock()
+        self.aborted.load(Ordering::Relaxed)
     }
 
     pub fn broadcast(&self, tensor: &mut dyn RawBaguaTensor, root_rank: i32) {
