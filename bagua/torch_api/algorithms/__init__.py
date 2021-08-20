28c85c3cc2ce70167656850e40ccf05015670d7d
ritaw <wangraying@gmail.com>
2021-08-20 12:02:04 +0800

    fmt

--- bagua/torch_api/algorithms/__init__.py
+++ bagua/torch_api/algorithms/__init__.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
 from .base import Algorithm  # noqa: F401
-from . import ( #noqa: F401
-    bytegrad,  # noqa: F401
-    decentralized,  # noqa: F401
-    gradient_allreduce,  # noqa: F401
-    q_adam,  # noqa: F401
-    async_model_average,  # noqa: F401
-)
+from . import bytegrad, decentralized, gradient_allreduce  # noqa: F401
+from . import q_adam, async_model_average  # noqa: F401
