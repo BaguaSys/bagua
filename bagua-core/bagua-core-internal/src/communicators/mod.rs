2552fac4f07156ebf8423b4ab92fb40208146f10
Rui Wang <45031995+wangraying@users.noreply.github.com>
2021-08-30 15:55:32 +0800

    Revert "fix: async algorithm abort abnormally" (#80)

--- bagua-core/bagua-core-internal/src/communicators/mod.rs
+++ bagua-core/bagua-core-internal/src/communicators/mod.rs
@@ -456,7 +456,7 @@ impl BaguaCommunicatorInner {
     pub fn abort(&self) {
         let communicator_ptr = self.comm_ptr;
 
-        self.set_abort();
+        self.aborted.store(true, Ordering::Relaxed);
 
         unsafe {
             cpp::cpp!([communicator_ptr as "Al::NCCLCommunicator*"]
@@ -464,10 +464,6 @@ impl BaguaCommunicatorInner {
                 communicator_ptr->abort();
             });
         }
-    }
-
-    pub fn set_abort(&self) {
-        self.aborted.store(true, Ordering::Relaxed);
     }
 
     pub fn check_abort(&self) -> bool {
