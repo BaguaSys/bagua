d3651e1e66fe602f855ea050cef2c9c28189833b
liuhatry <liuhatry@163.com>
2021-09-17 18:06:22 +0800

    update

--- bagua/torch_api/moe/__init__.py
+++ bagua/torch_api/moe/__init__.py
@@ -1,8 +1,3 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-#
-# This source code is licensed under the BSD license found in the
-# LICENSE file in the root directory of this source tree.
-
 from typing import List
 
 from .moe_layer import MOELayer
