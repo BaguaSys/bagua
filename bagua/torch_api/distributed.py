a8661b8f7b783584e7d4d301034177656e992750
suntengxu <suntengxu@kuaishou.com>
2021-09-22 16:57:10 +0800

    change license

--- bagua/torch_api/distributed.py
+++ bagua/torch_api/distributed.py
@@ -98,9 +98,21 @@ class BaguaModule:
 
         return parameters
 
-    # Copyright (c) Uber Technologies, Inc. and its affiliates.
+    # Copyright 2020 Uber Technologies, Inc. All Rights Reserved.
     # Copyright (c) 2021 Kuaishou AI Platform & DS3 Lab.
-    # All rights reserved.
+    #
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+    #
+    #     http://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+    # limitations under the License.
+    # ==============================================================================
     def _bagua_broadcast_optimizer_state(self, optimizer):
         # L-BFGS cannot be easily supported without serializing
         # the entire state_dict, as its structure is deeply nested and contains
