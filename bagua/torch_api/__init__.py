d8b50c375847fee7b4fdab217446084fe3b61b4e
liuhatry <liuhatry@163.com>
2021-09-30 09:18:33 +0800

    update

--- bagua/torch_api/__init__.py
+++ bagua/torch_api/__init__.py
@@ -57,5 +57,5 @@ from .env import (  # noqa: F401
 from . import contrib  # noqa: F401
 from . import communication  # noqa: F401
 from . import algorithms  # noqa: F401
-from . import checkpointing  # noqa: E402,F401
+from . import checkpoint  # noqa: E402,F401
 from .model_parallel import moe  # noqa: E402,F401
---
+++ bagua/torch_api/checkpoint/__init__.py
@@ -0,0 +1,2 @@
+from .checkpointing import load_checkpoint
+from .checkpointing import save_checkpoint
