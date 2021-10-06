d91aa579c9b96275d4827c2567b53218eb7058e7
suntengxu <suntengxu@kuaishou.com>
2021-10-07 00:05:52 +0800

    fix error

--- bagua/torch_api/algorithms_implementation/__init__.py
+++ bagua/torch_api/algorithms_implementation/__init__.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
 from .base import Algorithm  # noqa: F401
-from . import (
-    bytegrad_implementation,  # noqa: F401
-    decentralized_implementation,  # noqa: F401
-    gradient_allreduce_implementation,  # noqa: F401
-)  # noqa: F401
+from . import bytegrad_implementation, decentralized_implementation, gradient_allreduce_implementation  # noqa: F401
 from . import q_adam_implementation, async_model_average_implementation  # noqa: F401
