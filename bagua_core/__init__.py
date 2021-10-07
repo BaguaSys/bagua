f35ada75c62b28b51d1b844d3ec33c2508e0a082
Xiangru Lian <admin@mail.xrlian.com>
2021-10-07 02:45:44 -0700

    update

--- bagua_core/__init__.py
+++ bagua_core/__init__.py
@@ -2,4 +2,5 @@ from . import _environment
 
 _environment._preload_libraries()
 from .bagua_core import *  # noqa: F401,E402,F403
-from .bagua_install_deps import install_deps
+from .bagua_install_deps import install_deps  # noqa: F401,E402,F403
+
