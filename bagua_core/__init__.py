87b73acfbc70355b60b75279a6d815bf98b77685
NOBLES5E <NOBLES5E@users.noreply.github.com>
2021-10-07 09:46:42 +0000

    :art: Format Python code with psf/black

--- bagua_core/__init__.py
+++ bagua_core/__init__.py
@@ -3,4 +3,3 @@ from . import _environment
 _environment._preload_libraries()
 from .bagua_core import *  # noqa: F401,E402,F403
 from .bagua_install_deps import install_deps  # noqa: F401,E402,F403
-
