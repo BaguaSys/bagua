489fbd0690cc314c35d9b54f5ac48aae56e20fa2
Xiangru Lian <admin@mail.xrlian.com>
2021-10-07 01:35:45 -0700

    update

--- setup.py
+++ setup.py
@@ -4,147 +4,11 @@ from distutils.errors import (
     DistutilsPlatformError,
 )
 from setuptools_rust import Binding, RustExtension
-import platform
 import shutil
 import sys
-import tempfile
-import urllib.request
-from tqdm import tqdm
 from typing import List
 
-
-_nccl_records = []
 library_records = {}
-
-
-class DownloadProgressBar(tqdm):
-    def update_to(self, b=1, bsize=1, tsize=None):
-        if tsize is not None:
-            self.total = tsize
-        self.update(b * bsize - self.n)
-
-
-def download_url(url, output_path):
-    with DownloadProgressBar(
-        unit="B", unit_scale=True, miniters=1, desc=url.split("/")[-1]
-    ) as t:
-        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
-
-
-def _make_nccl_url(public_version, filename):
-    # https://developer.download.nvidia.com/compute/redist/nccl/v2.8/nccl_2.8.4-1+cuda11.2_x86_64.txz
-    return (
-        "https://developer.download.nvidia.com/compute/redist/nccl/"
-        + "v{}/{}".format(public_version, filename)
-    )
-
-
-def _make_nccl_record(cuda_version, full_version, public_version, filename_linux):
-    return {
-        "cuda": cuda_version,
-        "nccl": full_version,
-        "assets": {
-            "Linux": {
-                "url": _make_nccl_url(public_version, filename_linux),
-                "filename": "libnccl.so.{}".format(full_version),
-            },
-        },
-    }
-
-
-_nccl_records.append(
-    _make_nccl_record("11.4", "2.10.3", "2.10", "nccl_2.10.3-1+cuda11.4_x86_64.txz")
-)
-_nccl_records.append(
-    _make_nccl_record("11.3", "2.10.3", "2.10", "nccl_2.10.3-1+cuda11.0_x86_64.txz")
-)
-_nccl_records.append(
-    _make_nccl_record("11.2", "2.10.3", "2.10", "nccl_2.10.3-1+cuda11.0_x86_64.txz")
-)
-_nccl_records.append(
-    _make_nccl_record("11.1", "2.10.3", "2.10", "nccl_2.10.3-1+cuda11.0_x86_64.txz")
-)
-_nccl_records.append(
-    _make_nccl_record("11.0", "2.10.3", "2.10", "nccl_2.10.3-1+cuda11.0_x86_64.txz")
-)
-_nccl_records.append(
-    _make_nccl_record("10.2", "2.10.3", "2.10", "nccl_2.10.3-1+cuda10.2_x86_64.txz")
-)
-_nccl_records.append(
-    _make_nccl_record("10.1", "2.10.3", "2.10", "nccl_2.10.3-1+cuda10.2_x86_64.txz")
-)
-library_records["nccl"] = _nccl_records
-
-
-def install_baguanet(destination):
-    os.system("cd rust/bagua-net/cc && make")
-    shutil.move("rust/bagua-net/cc/libnccl-net.so", destination)
-
-
-def install_lib(cuda, prefix, library):
-    if prefix is None:
-        prefix = os.path.expanduser("~/.bagua_core/cuda_lib")
-
-    destination = os.path.join(prefix, ".data")
-
-    if library == "bagua-net":
-        dst_dir = os.path.join(destination, "bagua-net")
-        os.makedirs(dst_dir, exist_ok=True)
-        install_baguanet(dst_dir)
-        print("Done!")
-        return
-
-    record = None
-    lib_records = library_records
-    for record in lib_records[library]:
-        if record["cuda"] == cuda:
-            break
-    else:
-        raise RuntimeError(
-            """
-The CUDA version({}) specified is not supported.
-Should be one of {}.""".format(
-                cuda, str([x["cuda"] for x in lib_records[library]])
-            )
-        )
-
-    if os.path.exists(destination):
-        print("The destination directory {} already exists.".format(destination))
-        shutil.rmtree(destination)
-
-    target_platform = platform.system()
-    asset = record["assets"].get(target_platform, None)
-    if asset is None:
-        raise RuntimeError(
-            """
-The current platform ({}) is not supported.""".format(
-                target_platform
-            )
-        )
-
-    print(
-        "Installing {} {} for CUDA {} to: {}".format(
-            library, record[library], record["cuda"], destination
-        )
-    )
-
-    url = asset["url"]
-    print("Downloading {}...".format(url))
-    with tempfile.TemporaryDirectory() as tmpdir:
-        filename = os.path.join(tmpdir, os.path.basename(url))
-        download_url(url, filename)
-        print("Extracting...")
-        outdir = os.path.join(tmpdir, "extract")
-        shutil.unpack_archive(filename, outdir)
-        print("Installing...")
-        if library == "nccl":
-            subdir = os.listdir(outdir)
-            assert len(subdir) == 1
-            shutil.move(os.path.join(outdir, subdir[0]), destination)
-        else:
-            assert False
-        print("Cleaning up...")
-    print("Done!")
 
 
 def check_torch_version():
@@ -162,6 +26,12 @@ def check_torch_version():
     return version
 
 
+def install_baguanet(destination):
+    os.makedirs(destination, exist_ok=True)
+    os.system("cd rust/bagua-net/cc && make")
+    shutil.move("rust/bagua-net/cc/libnccl-net.so", destination)
+
+
 def install_dependency_library():
     nvcc_version = (
         os.popen(
@@ -171,10 +41,7 @@ def install_dependency_library():
         .strip()
     )
     print("nvcc_version: ", nvcc_version)
-    if int(os.getenv("BAGUA_NO_INSTALL_BAGUA_NET", 0)) == 0:
-        install_lib(nvcc_version, os.path.join(cwd, "bagua_core"), "bagua-net")
-    if int(os.getenv("BAGUA_NO_INSTALL_NCCL", 0)) == 0:
-        install_lib(nvcc_version, os.path.join(cwd, "bagua_core"), "nccl")
+    install_baguanet(os.path.join(cwd, "bagua_core", ".data", "bagua-net"))
 
 
 if __name__ == "__main__":
@@ -197,7 +64,7 @@ if __name__ == "__main__":
         print(
             colorama.Fore.BLACK
             + colorama.Back.CYAN
-            + "Bagua is automatically installing some system dependencies like NCCL, to disable set env variable BAGUA_NO_INSTALL_DEPS=1",
+            + "Bagua is automatically installing some system dependencies like bagua-net, to disable set env variable BAGUA_NO_INSTALL_DEPS=1",
         )
         install_dependency_library()
 
@@ -215,7 +82,6 @@ if __name__ == "__main__":
         packages=find_packages(exclude=("tests")),
         package_data={
             "": [
-                ".data/lib/libnccl.so",
                 ".data/bagua-net/libbagua_net.so",
                 ".data/bagua-net/libnccl-net.so",
             ]
