74a029a651352bfc6ea496f5e6250360b5d7f445
Xiangru Lian <admin@mail.xrlian.com>
2021-10-07 02:31:25 -0700

    chore: update

--- bagua_core/_environment.py
+++ bagua_core/_environment.py
@@ -8,4 +8,4 @@ def _preload_libraries():
     if os.path.exists(libnccl_path):
         ctypes.CDLL(libnccl_path)
     else:
-        logging.warning("Bagua cannot detect bundled NCCL library, trying to use system NCCL instead. If you encounter any error, please run `import bagua_core; bagua_core.install_deps()` or the `bagua_install_deps.py` script to install bundled libraries.")
+        logging.warning("Bagua cannot detect bundled NCCL library, Bagua will try to use system NCCL instead. If you encounter any error, please run `import bagua_core; bagua_core.install_deps()` or the `bagua_install_deps.py` script to install bundled libraries.")
