ad4890d6e8367f48b6e4b28652676639fa2c880b
Xiangru Lian <admin@mail.xrlian.com>
2021-10-07 23:00:30 -0700

    update

--- bagua/torch_api/communication.py
+++ bagua/torch_api/communication.py
@@ -74,6 +74,8 @@ def get_backend(model_name: str):
 
 def run_flask_app():
     from flask import Flask
+    import os
+    os.environ["FLASK_ENV"] = "development"
 
     autotune_service = AutotuneService(
         world_size=get_world_size(),
