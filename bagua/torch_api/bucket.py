cc3c7154e678bac9482fa42a7b9ca08471aef2de
ritaw <wangraying@gmail.com>
2021-10-17 09:09:49 +0800

    fix

--- bagua/torch_api/bucket.py
+++ bagua/torch_api/bucket.py
@@ -93,12 +93,6 @@ class BaguaBucket:
             device=self._all_tensors[0].device,
         )
 
-        if self.check_flatten():
-            flatten_tensor.set_(
-                self._all_tensors[0].data.storage(), 0, flatten_tensor.shape
-            )
-            return flatten_tensor
-
         offset = 0
         for tensor in self._all_tensors:
             # copy data
@@ -110,21 +104,15 @@ class BaguaBucket:
         """
         Flatten inner tensors in place.
         """
-        if self.check_flatten():
-            self.backend_tensor = self.flattened_tensor()
-            return
-
         if len(self._all_tensors) == 0:
             return
-        total_size = 0
-        for tensor in self._all_tensors:
-            total_size += tensor.numel()
 
-        flatten_tensor = torch.zeros(
-            total_size,
-            dtype=self._all_tensors[0].dtype,
-            device=self._all_tensors[0].device,
-        )
+        flatten_tensor = self.flattened_tensor()
+
+        if self.check_flatten():
+            flatten_tensor.set_(self._all_tensors[0].storage(), 0, flatten_tensor.shape)
+            self.backend_tensor = flatten_tensor
+            return
 
         flatten_storage = flatten_tensor.storage()
 
