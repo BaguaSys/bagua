c7cc15bfbb836d05a0e3e71c01f872c4c62adcc6
liuhatry <liuhatry@163.com>
2021-10-19 17:55:11 +0800

    Update bagua/torch_api/model_parallel/moe/megatron/layers.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- bagua/torch_api/model_parallel/moe/megatron/layers.py
+++ bagua/torch_api/model_parallel/moe/megatron/layers.py
@@ -7,7 +7,8 @@ class MoeBaseMLP(torch.nn.Module):
         from megatron import get_args
 
 
-        args = get_args()
+
+
 
         self.dense_h_to_4h = torch.nn.Linear(args.hidden_size, args.ffn_hidden_size)
         self.dense_4h_to_h = torch.nn.Linear(args.ffn_hidden_size, args.hidden_size)
