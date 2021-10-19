f38b8e7efb43f88829ddaa3a5bc2aef21f823803
root <root@bjlt-hg96.sy>
2021-10-19 18:05:39 +0800

    udpate

--- bagua/torch_api/model_parallel/moe/megatron/layers.py
+++ bagua/torch_api/model_parallel/moe/megatron/layers.py
@@ -5,10 +5,7 @@ class MoeBaseMLP(torch.nn.Module):
     def __init__(self):
         super(MoeBaseMLP, self).__init__()
         from megatron import get_args
-
-
-
-
+        args = get_args()
 
         self.dense_h_to_4h = torch.nn.Linear(args.hidden_size, args.ffn_hidden_size)
         self.dense_4h_to_h = torch.nn.Linear(args.ffn_hidden_size, args.hidden_size)
