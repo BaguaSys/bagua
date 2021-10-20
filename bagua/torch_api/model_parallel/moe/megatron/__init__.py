109ba3a781b804c53dfec451d95ed22e57c5df63
liuhatry <liuhatry@163.com>
2021-10-20 10:17:28 +0800

    feat(python): add utils for megatron-moe

---
+++ bagua/torch_api/model_parallel/moe/megatron/__init__.py
@@ -0,0 +1,4 @@
+from .checkpointing import save_checkpoint  # noqa: F401
+from .checkpointing import load_checkpoint  # noqa: F401
+from .layers import MoeBaseMLP  # noqa: F401
+from .utils import _add_moe_args  # noqa: F401
