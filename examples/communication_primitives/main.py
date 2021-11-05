8d59621de093b340c4d939f43a94176a87ff3e59
jiangyouhe <youhejiang@gmail.com>
2021-11-05 14:13:38 +0800

    support all_to_all_single

--- examples/communication_primitives/main.py
+++ examples/communication_primitives/main.py
@@ -146,31 +146,5 @@ def main():
         a=recv_tensors, b=recv_tensor_bagua
     )
 
-    # alltoall_v
-    send_tensors = torch.arange(9, dtype=torch.float32).cuda()
-    recv_tensors = torch.zeros(9, dtype=torch.float32).cuda()
-    recv_tensor_bagua = torch.zeros(9, dtype=torch.float32).cuda()
-    in_splits = [1, 1, 1, 1, 1, 1, 1, 2]
-    out_splits = [1, 1, 1, 1, 1, 1, 1, 2]
-    send_counts = np.array([1, 1, 1, 1, 1, 1, 1, 2])
-    recv_counts = np.array([1, 1, 1, 1, 1, 1, 1, 2])
-    send_sdispls = np.array([0, 1, 2, 3, 4, 5, 6, 7, 9])
-    recv_sdispls = np.array([0, 1, 2, 3, 4, 5, 6, 7, 9])
-    dist.all_to_all_single(recv_tensors, send_tensors, out_splits, in_splits)
-    bagua.alltoall_v(
-        send_tensors,
-        send_counts,
-        send_sdispls,
-        recv_tensor_bagua,
-        recv_counts,
-        recv_sdispls,
-        comm=comm,
-    )
-    assert torch.equal(
-        recv_tensors, recv_tensor_bagua
-    ), "recv_tensors:{a}, recv_tensor_bagua:{b}".format(
-        a=recv_tensors, b=recv_tensor_bagua
-    )
-
 if __name__ == "__main__":
     main()
