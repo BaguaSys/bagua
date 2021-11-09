71b6b429a247f156adcc09fe7234a1cfe941c36a
Youhe-Jiang <85312798+Youhe-Jiang@users.noreply.github.com>
2021-11-09 11:38:54 +0800

    feat(python, core): support all_to_all_single (#361)

--- rust/bagua-core/bagua-core-py/src/lib.rs
+++ rust/bagua-core/bagua-core-py/src/lib.rs
@@ -136,6 +136,16 @@ impl BaguaSingleCommunicatorPy {
         )
     }
 
+    pub fn alltoall_v_inplace(
+        &self,
+        tensor: &mut BaguaTensorPy,
+        counts: Vec<usize>,
+        displs: Vec<usize>,
+    ) {
+        self.inner
+            .alltoall_v_inplace(&mut tensor.inner, &counts, &displs)
+    }
+
     pub fn allgather(&self, send_tensor: &BaguaTensorPy, recv_tensor: &mut BaguaTensorPy) {
         self.inner
             .allgather(&send_tensor.inner, &mut recv_tensor.inner)
