67cb898cc39e3b9e519ee5dccdb2864d6febe074
shjwudp <shjwudp@gmail.com>
2021-11-19 17:34:12 +0800

    chore: add should be skipped moe param

--- bagua/torch_api/model_parallel/moe/utils.py
+++ bagua/torch_api/model_parallel/moe/utils.py
@@ -2,6 +2,23 @@ import torch
 
 
 def is_moe_param(param: torch.Tensor) -> bool:
+    """The MOE parameter should be skip allreduce.
+
+    Args:
+        param (torch.Tensor): torch parameter
+
+    Returns:
+        bool: [description]
+    """
+    dp_comm = None
+    if hasattr(param, "dp_comm"):
+        dp_comm = param.dp_comm
+    else:
+        dp_comm = "dp"
+
+    if dp_comm is None:
+        return True
+
     if hasattr(param, "expert") and param.expert:
         return True
     return False
