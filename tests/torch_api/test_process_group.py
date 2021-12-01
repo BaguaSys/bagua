600860f691de5d3e402621e3e9ba6ec0175eee78
shjwudp <shjwudp@gmail.com>
2021-12-01 09:17:22 +0800

    feat(python): associate PyTorch Process Group with Bagua Process Group using cache (#402)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/torch_api/test_process_group.py
+++ tests/torch_api/test_process_group.py
@@ -1,6 +1,11 @@
-import bagua.torch_api as bagua
-import torch
+import os
 import unittest
+
+import torch
+import torch.distributed as c10d
+
+import bagua.torch_api as bagua
+from tests.internal.common_utils import find_free_port
 from tests.internal.multi_process import MultiProcessTestCase, setup_bagua_env
 from tests import skip_if_cuda_not_available
 
@@ -71,6 +76,51 @@ class TestProcessGroup(MultiProcessTestCase):
     def test_from_torch_group(self):
         nprocs = torch.cuda.device_count()
         self.run_test_locally(run_from_torch_group, nprocs, args={}, results=None)
+
+
+from torch.testing._internal.common_distributed import (  # noqa: E402
+    MultiProcessTestCase,
+    skip_if_lt_x_gpu,
+)
+
+
+class ProcessGroupNCCLTest(MultiProcessTestCase):
+    def setUp(self):
+        super(ProcessGroupNCCLTest, self).setUp()
+        # NCCL_BLOCKING_WAIT overrides NCCL_ASYNC_ERROR_HANDLING hence tests
+        # that use NCCL_BLOCKING_WAIT will test it as expected.
+        os.environ["NCCL_ASYNC_ERROR_HANDLING"] = "1"
+        os.environ.update(
+            {
+                "MASTER_ADDR": "127.0.0.1",
+                "MASTER_PORT": str(find_free_port(8000, 8100)),
+                "BAGUA_SERVICE_PORT": str(find_free_port(9000, 9100)),
+            }
+        )
+        self._spawn_processes()
+
+    @skip_if_lt_x_gpu(2)
+    def test_bagua_pg(self):
+        # Need to use NCCL_BLOCKING_WAIT and not ASYNC_ERROR_HANDLING,
+        # otherwise process will be taken down and we can't check for errors.
+        os.environ["NCCL_ASYNC_ERROR_HANDLING"] = "0"
+        os.environ["NCCL_BLOCKING_WAIT"] = "1"
+        os.environ.update(
+            {
+                "WORLD_SIZE": str(self.world_size),
+                "LOCAL_WORLD_SIZE": str(self.world_size),
+                "RANK": str(self.rank),
+                "LOCAL_RANK": str(self.rank),
+            }
+        )
+
+        bagua.init_process_group()
+        pg = c10d.new_group(ranks=list(range(0, self.world_size)))
+        pg.bagua_patch()
+        self.assertTrue(pg in bagua.communication._torch_to_bagua_pg_map)
+        del pg
+        c10d.destroy_process_group()
+        self.assertEqual(len(bagua.communication._torch_to_bagua_pg_map), 0)
 
 
 if __name__ == "__main__":
