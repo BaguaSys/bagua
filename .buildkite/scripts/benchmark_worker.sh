ed54511887d9360fa376ee46c2caf164e13bc131
Rui Wang <45031995+wangraying@users.noreply.github.com>
2022-01-01 09:53:41 +0800

    ci: fix ci (#466)

--- .buildkite/scripts/benchmark_worker.sh
+++ .buildkite/scripts/benchmark_worker.sh
@@ -12,7 +12,7 @@ export HOME=/workdir && cd $HOME && bash .buildkite/scripts/install_bagua.sh || 
 # 1. test communication_primitives api
 echo "begin to test [communication_primitives]"
 COMMUNICATION_SCRIPT="/workdir/examples/communication_primitives/main.py"
-python -m bagua.distributed.launch \
+NCCL_SOCKET_IFNAME=^docker,lo,veth python -m bagua.distributed.launch \
     --nnodes=2 \
     --nproc_per_node 4 \
     --node_rank=1 \
@@ -27,7 +27,7 @@ length=${#algorithms[@]}
 for ((i = 0; i < $length; i++)); do
     echo "begin to test ["${algorithms[$i]}]
     logfile=$(mktemp /tmp/bagua_benchmark_${algorithms[$i]}.XXXXXX.log)
-    GLOO_SOCKET_IFNAME=enp96s0f0 python -m bagua.distributed.launch \
+    NCCL_SOCKET_IFNAME=^docker,lo,veth GLOO_SOCKET_IFNAME=enp96s0f0 python -m bagua.distributed.launch \
         --nnodes=2 \
         --nproc_per_node 4 \
         --node_rank=1 \
@@ -45,7 +45,7 @@ done
 # 3. test moe
 MOE_SCRIPT="/workdir/examples/moe/mnist_main.py"
 logfile=$(mktemp /tmp/bagua_moe_gradient_allreduce.XXXXXX.log)
-CUDA_VISIBLE_DEVICES=0,1 python -m bagua.distributed.launch \
+NCCL_SOCKET_IFNAME=^docker,lo,veth CUDA_VISIBLE_DEVICES=0,1 python -m bagua.distributed.launch \
     --nnodes=2 \
     --nproc_per_node 2 \
     --node_rank=1 \
