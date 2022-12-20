3373070e64d8c6041b74aeb1283a8e000736aa19
Jianbin Chang <shjwudp@gmail.com>
2022-12-20 18:50:59 +0800

    fix: Fix incorrect rust deps and setuptools environment prepare.

--- .buildkite/scripts/run_pytest.sh
+++ .buildkite/scripts/run_pytest.sh
@@ -5,6 +5,9 @@ echo "$BUILDKITE_PARALLEL_JOB_COUNT"
 
 set -euo pipefail
 cp -a /upstream /workdir
+# setuptools will reset "$HOME" to the setup.py execution directory, set dependency of rust at new "$HOME" to ensure that rustc can run normally.
+ln -s $HOME/.rustup /workdir
+ln -s $HOME/.cargo /workdir
 export HOME=/workdir && cd $HOME && bash .buildkite/scripts/install_bagua.sh || exit 1
 pip install pytest-timeout
 pip install git+https://github.com/PyTorchLightning/pytorch-lightning.git
