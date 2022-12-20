478f6974d030f4f3cac521d41aa72a1cf67a6a8c
Jianbin Chang <shjwudp@gmail.com>
2022-12-20 19:20:43 +0800

    .

--- .buildkite/scripts/run_pytest.sh
+++ .buildkite/scripts/run_pytest.sh
@@ -8,7 +8,7 @@ cp -a /upstream /workdir
 # setuptools will reset "$HOME" to the setup.py execution directory, set dependency of rust at new "$HOME" to ensure that rustc can run normally.
 ln -s $HOME/.rustup /workdir
 ln -s $HOME/.cargo /workdir
-export HOME=/workdir && cd $HOME && bash .buildkite/scripts/install_bagua.sh || exit 1
+bash .buildkite/scripts/install_bagua.sh || exit 1
 pip install pytest-timeout
 pip install git+https://github.com/PyTorchLightning/pytorch-lightning.git
 pytest --timeout=300 -s -o "testpaths=tests"
