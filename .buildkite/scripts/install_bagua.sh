a61c9acdf6b1dd0ea85a45953735e99fd0c28df0
Jianbin Chang <shjwudp@gmail.com>
2022-12-20 19:33:00 +0800

    fix: Fix cover benchmark.sh

--- .buildkite/scripts/install_bagua.sh
+++ .buildkite/scripts/install_bagua.sh
@@ -5,7 +5,15 @@ echo "$BUILDKITE_PARALLEL_JOB_COUNT"
 
 set -euox pipefail
 
+SOURCE_DIR=$1
+WORK_DIR=$2
+
+cp -a $SOURCE_DIR $WORK_DIR
+# setuptools will reset "$HOME" to the setup.py execution directory, set dependency of rust at new "$HOME" to ensure that rustc can run normally.
+ln -s $HOME/.rustup $WORK_DIR
+ln -s $HOME/.cargo $WORK_DIR
+
 pip uninstall -y bagua bagua-core
-git config --global --add safe.directory /workdir/rust/bagua-core/bagua-core-internal/third_party/Aluminum
-cd /workdir && python3 setup.py install -f || exit 1
+git config --global --add safe.directory $WORK_DIR/rust/bagua-core/bagua-core-internal/third_party/Aluminum
+cd $WORK_DIR && python3 setup.py install -f || exit 1
 rm -rf bagua bagua_core
--- .buildkite/scripts/run_pytest.sh
+++ .buildkite/scripts/run_pytest.sh
@@ -4,11 +4,8 @@ echo "$BUILDKITE_PARALLEL_JOB"
 echo "$BUILDKITE_PARALLEL_JOB_COUNT"
 
 set -euo pipefail
-cp -a /upstream /workdir
-# setuptools will reset "$HOME" to the setup.py execution directory, set dependency of rust at new "$HOME" to ensure that rustc can run normally.
-ln -s $HOME/.rustup /workdir
-ln -s $HOME/.cargo /workdir
-bash .buildkite/scripts/install_bagua.sh || exit 1
+
+bash .buildkite/scripts/install_bagua.sh /upstream /workdir || exit 1
 pip install pytest-timeout
 pip install git+https://github.com/PyTorchLightning/pytorch-lightning.git
 pytest --timeout=300 -s -o "testpaths=tests"
